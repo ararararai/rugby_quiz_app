d73abe334b693620f06dacbcf9c713a996b27a36
ararararai <usd1122334@gmail.com>
2025-03-29 15:34:45 +0900

    チームロゴが表示しないエラーを解消

--- database/seeders/LeagueOneTeamsSeeder.php
+++ database/seeders/LeagueOneTeamsSeeder.php
@@ -10,18 +10,18 @@ class LeagueOneTeamsSeeder extends Seeder
     public function run(): void
     {
         $teams = [
-            ['name' => 'サントリーサンゴリアス', 'logo_path' => 'teams/sungoliath.jpg'],
-            ['name' => '東芝ブレイブルーパス東京', 'logo_path' => 'teams/placeholder.jpg'],
-            ['name' => 'クボタスピアーズ船橋・東京ベイ', 'logo_path' => 'teams/placeholder.jpg'],
-            ['name' => '静岡ブルーレブズ', 'logo_path' => 'teams/placeholder.jpg'],
-            ['name' => '神戸製鋼コベルコスティーラーズ神戸', 'logo_path' => 'teams/placeholder.jpg'],
-            ['name' => '浦安D-Rocks', 'logo_path' => 'teams/placeholder.jpg'],
-            ['name' => '横浜キヤノンイーグルス', 'logo_path' => 'teams/placeholder.jpg'],
-            ['name' => '埼玉ワイルドナイツ', 'logo_path' => 'teams/placeholder.jpg'],
-            ['name' => 'ブラックラムズ東京', 'logo_path' => 'teams/placeholder.jpg'],
-            ['name' => '三重ホンダヒート', 'logo_path' => 'teams/placeholder.jpg'],
-            ['name' => 'トヨタグリーンロケッツ', 'logo_path' => 'teams/placeholder.jpg'],
-            ['name' => '三菱重工相模原ダイナボアーズ', 'logo_path' => 'teams/placeholder.jpg']
+            ['name' => 'サントリーサンゴリアス', 'logo_path' => 'teams/logo_sungoliath.png'],
+            ['name' => '東芝ブレイブルーパス東京', 'logo_path' => 'teams/logo_bravelupus.png'],
+            ['name' => 'クボタスピアーズ船橋・東京ベイ', 'logo_path' => 'teams/logo_spears.png'],
+            ['name' => '静岡ブルーレブズ', 'logo_path' => 'teams/logo_bluerevs.png'],
+            ['name' => '神戸製鋼コベルコスティーラーズ神戸', 'logo_path' => 'teams/logo_kobesteelers.png'],
+            ['name' => '浦安D-Rocks', 'logo_path' => 'teams/logo_d-rocks.png'],
+            ['name' => '横浜キヤノンイーグルス', 'logo_path' => 'teams/logo_eagles.png'],
+            ['name' => '埼玉ワイルドナイツ', 'logo_path' => 'teams/logo_verblitz.png'],
+            ['name' => 'ブラックラムズ東京', 'logo_path' => 'teams/logo_blackrams.png'],
+            ['name' => '三重ホンダヒート', 'logo_path' => 'teams/logo_heat.png'],
+            ['name' => 'トヨタグリーンロケッツ', 'logo_path' => 'teams/logo_panasonic.png'],
+            ['name' => '三菱重工相模原ダイナボアーズ', 'logo_path' => 'teams/logo_dynaboars.png']
         ];
 
         foreach ($teams as $team) {
